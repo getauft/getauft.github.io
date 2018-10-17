d5cfe8ea6b74ce5c4a255005fdfb91026e3e81eb
Anton Lavrinenko <getauft@gmail.com>
2018-10-17 13:53:29 +0300

    1

---
+++ js/pronouns.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var place_pronoun_items = document.getElementById("place_pronoun_items");
+
+var html = "";
+
+pronouns.forEach(function (pronoun){
+    html +=
+        '<div class="row">'+
+            '<div class="col-md">'+
+                '<strong>'+pronoun.subject_pronoun+'</strong>'+
+                ' '+
+                pronoun.person+
+                ' '+
+                pronoun.object_pronoun+
+                ' '+
+                pronoun.possessive_adjective+
+                ' '+
+                pronoun.possessive_pronoun+
+                ' '+
+                pronoun.reflexive_or_intensive_pronoun+
+            '</div>'+
+        '</div>'
+    ;
+    i++;
+});
+place_verb_items.innerHTML = html;
