6911e1bdd4e42190ebb7c35f9fd1f4db55d0c489
Anton Lavrinenko <getauft@gmail.com>
2018-10-17 16:30:10 +0300

    1

--- js/verbs.js
+++ js/verbs.js
@@ -3,18 +3,15 @@
 var place_verb_items = document.getElementById("place_verb_items");
 
 var html = "";
-var i = 1;
 
 verbs.forEach(function (verb){
     html +=
         '<tr>'+
-            '<th>'+i+'.</th>'+
             '<th>'+verb.base_form+'</th>'+
             '<th>'+verb.simple_past+'</th>'+
             '<th>'+verb.past_participle+'</th>'+
             '<th>'+verb.translate+'</th>'+
         '</tr>'
     ;
-    i++;
 });
 place_verb_items.innerHTML = html;
